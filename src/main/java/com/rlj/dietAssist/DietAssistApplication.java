8eda82fc4307c652868cf02dcdd4f6cbd1445d54
rlj_su <geunsu902@gmail.com>
2025-01-26 13:23:37 +0900

    build project

---
+++ src/main/java/com/rlj/dietAssist/DietAssistApplication.java
@@ -0,0 +1,13 @@
+package com.rlj.dietAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietAssistApplication.class, args);
+	}
+
+}
